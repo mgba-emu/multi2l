b726e0d17ba243f7b45a6a26f619049146ad3965
Vicki Pfau <vi@endrift.com>
2017-12-17 22:52:04 -0800

    ROM size detection

--- source/main.c
+++ source/main.c
@@ -18,6 +18,7 @@ static char cartTitle[13] = "";
 static struct SaveCharacteristics cartSave = {};
 static int cartSensors = 0;
 static int cartLight = 0;
+static u32 cartSize = 0;
 
 static void updateTextGrid(void) {
 	int i;
@@ -46,73 +47,89 @@ static void cartridgeHeartbeat(void) {
 
 	vu8* gbarom = (vu8*) GBA_BUS;
 	u8 b4 = gbarom[0xb4];
-	if (b4) {
+	if (b4 == 0xDA) {
 		cartInserted = false;
 		return;
 	}
 
-	vu16* hibase = (vu16*) (gbarom + 0x1FFFFE0);
-	hibase[0];
-	hibase[1];
-	hibase[2];
-	hibase[3];
-	hibase[4];
-	hibase[5];
-	hibase[6];
-	hibase[7];
-	hibase[8];
-	hibase[9];
+	if (!cartInserted) {
+		vu16* hibase = (vu16*) (gbarom + 0x1FFFFE0);
+		hibase[0];
+		hibase[1];
+		hibase[2];
+		hibase[3];
+		hibase[4];
+		hibase[5];
+		hibase[6];
+		hibase[7];
+		hibase[8];
+		hibase[9];
 
-	// 06AC
-	int x;
-	u32 hash = 0;
-	for (x = 0x9D; x < 0xB8; ++ x) {
-		u8 b = gbarom[x];
-		hash = (hash >> 3) | (hash << 29);
-		hash ^= b;
-		b <<= 8;
-		hash ^= b;
-		b <<= 8;
-		hash ^= b;
-		b <<= 8;
-		hash ^= b;
-	}
-	hash <<= 27;
-	hash >>= 30;
+		// 06AC
+		int x;
+		u32 hash = 0;
+		for (x = 0x9D; x < 0xB8; ++ x) {
+			u8 b = gbarom[x];
+			hash = (hash >> 3) | (hash << 29);
+			hash ^= b;
+			b <<= 8;
+			hash ^= b;
+			b <<= 8;
+			hash ^= b;
+			b <<= 8;
+			hash ^= b;
+		}
+		hash <<= 27;
+		hash >>= 30;
 
-	// 05B8
-	int tindex = 6 * hash + (gbarom[0x9E] & 3) * 0x18;
+		// 05B8
+		int tindex = 6 * hash + (gbarom[0x9E] & 3) * 0x18;
 
-	for (x = 2; x < 42; ++x) {
-		GBA_BUS[x];
-	}
+		for (x = 2; x < 42; ++x) {
+			GBA_BUS[x];
+		}
 
-	int y, z;
-	for (y = 0; y < 6; ++y) {
-		++tindex;
-		u32 address = table[tindex];
-		GBA_BUS[address];
+		int y, z;
+		for (y = 0; y < 6; ++y) {
+			++tindex;
+			u32 address = table[tindex];
+			GBA_BUS[address];
+			for (z = 10 * y; z < 10 * (y + 1); ++z) {
+				GBA_BUS[x + z];
+			}
+		}
 		for (z = 10 * y; z < 10 * (y + 1); ++z) {
 			GBA_BUS[x + z];
 		}
 	}
-	for (z = 10 * y; z < 10 * (y + 1); ++z) {
-		GBA_BUS[x + z];
-	}
 
 	if (!GBA_BUS[0]) {
 		cartInserted = false;
+		return;
 	}
 
-	if (!cartInserted) {
-		cartInserted = true;
+	cartInserted = true;
 
-		memcpy(cartCode, GBA_HEADER.gamecode, 6);
-		memcpy(cartTitle, GBA_HEADER.title, 12);
+	memcpy(cartCode, GBA_HEADER.gamecode, 6);
+	memcpy(cartTitle, GBA_HEADER.title, 12);
 
-		detectSaveType(&cartSave);
-		cartSensors = setupSensors();
-		cartLight = 0;
+	detectSaveType(&cartSave);
+	cartSensors = setupSensors();
+	cartLight = 0;
+	cartSize = 0;
+	int border = 0x800000;
+	int end;
+	for (end = 0; end < 4; ++end) {
+		u16 bytes = GBAROM[border];
+		if (bytes) {
+			cartSize = border << 2;
+			break;
+		}
+		if (GBAROM[border * 2 - 1] != 0xFFFF) {
+			cartSize = border << 2;
+			break;
+		}
+		border >>= 1;
 	}
 }
 
@@ -124,26 +141,26 @@ static void heartbeat(void) {
 		strcpy(&textGrid[0][8], "Inserted");
 		sprintf(&textGrid[0][32], "ID: %s", cartCode);
 		sprintf(&textGrid[0][64], "Title: %s", cartTitle);
-		strcpy(&textGrid[0][96], "Save type: ");
+		sprintf(&textGrid[0][96], "Size: %ld", cartSize);
+		strcpy(&textGrid[0][128], "Save type: ");
 		switch (cartSave.type) {
 		case SAVE_SRAM:
-			strcpy(&textGrid[0][107], "SRAM/FRAM");
+			strcpy(&textGrid[0][139], "SRAM/FRAM");
 			break;
 		case SAVE_FLASH:
-			sprintf(&textGrid[0][107], "Flash (%04X, %s)", cartSave.flashId, cartSave.flashMfg);
+			sprintf(&textGrid[0][139], "Flash (%04X, %s)", cartSave.flashId, cartSave.flashMfg);
 			break;
 		case SAVE_EEPROM:
-			strcpy(&textGrid[0][107], "EEPROM");
+			strcpy(&textGrid[0][139], "EEPROM");
 			break;
 		default:
-			strcpy(&textGrid[0][107], "None");
+			strcpy(&textGrid[0][139], "None");
 			break;
 		}
-		sprintf(&textGrid[0][128], "Save size: %ld", cartSave.size);
-		strcpy(&textGrid[0][160], "Sensors: ");
+		sprintf(&textGrid[0][160], "Save size: %ld", cartSave.size);
+		strcpy(&textGrid[0][192], "Sensors:");
 		if (cartSensors) {
-			strcpy(&textGrid[0][169], "Yes");
-			int tile = 196;
+			int tile = 226;
 			if (cartSensors & SENSOR_TILT) {
 				u16 x, y;
 				testTilt(&x, &y);
@@ -171,8 +188,9 @@ static void heartbeat(void) {
 				sprintf(&textGrid[0][tile], "Rumble: Present");
 				tile += 32;
 			}
+		} else {
+			strcpy(&textGrid[0][201], "None");
 		}
-
 	} else {
 		strcpy(&textGrid[0][8], "Empty");
 	}
@@ -181,11 +199,13 @@ static void heartbeat(void) {
 
 int main(void) {
 	videoSetMode(MODE_0_2D);
+	videoSetModeSub(MODE_0_2D);
 	vramSetPrimaryBanks(VRAM_A_MAIN_BG, VRAM_B_MAIN_SPRITE, VRAM_C_SUB_BG, VRAM_D_SUB_SPRITE);
 
 	sysSetCartOwner(BUS_OWNER_ARM9);
 
 	BG_PALETTE[0] = 0x7FFF;
+	BG_PALETTE_SUB[0] = 0x7FFF;
 	dmaCopyHalfWords(3, fontTiles, BG_GFX, fontTilesLen);
 	dmaCopyHalfWords(3, fontTiles, BG_GFX_SUB, fontTilesLen);
 
